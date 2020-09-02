cef98a682082ea23b6a01698104e397259464600
Joseph Perry <josephperry720@gmail.com>
2020-09-02 15:30:01 -0400

    added user propmts for password character options

--- script.js
+++ script.js
@@ -10,16 +10,18 @@ function writePassword() {
 
 }
 
-var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-console.log(letters.length)
-
 function generatePassword() {
   var master = [];
   var characters = "`~!@#$%^&*()_-+={}[]|':;<>,.?/"
   var digits = "0123456789"
   var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+  var upperChoice = confirm("Do you want upper case letters in the password?")
+  var lowerChoice = confirm("Do you want lower case letters in the password?")
+  var digitChoice = confirm("Do you want numbers in the password?")
+  var charChoice = confirm("Do you want special characters in the password?")
+
+  console.log(upperChoice, lowerChoice, digitChoice, charChoice)
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
